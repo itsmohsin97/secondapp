e2b3efb43e2e337f3624e3b3c9eb387554aba880
itsmohsin97 <mohsinsultan14k@gmail.com>
2020-10-26 12:46:48 +0530

    Adding first code change

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'hi this is my second app in angular :)';
+}
